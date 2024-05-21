1af84c1b2f292304a351acf6ce218e992326c742
white-duke <identifiantapple.ipad6@gmail.com>
2024-05-21 16:05:37 +0200

    chore: Update user avatar image path

--- src/configs/user.ts
+++ src/configs/user.ts
@@ -2,8 +2,8 @@ import type { UserData } from "~/types";
 
 const user: UserData = {
   name: "Whyte-Duke",
-  avatar: "img/ui/avatar.jpg",
-  password: import.meta.env.VITE_USER_PASSWORD || "" // Utilisation de la variable d'environnement
+  avatar: "img/ui/whyte-avatar-min.png",
+  password: import.meta.env.VITE_USER_PASSWORD || "" 
 };
 console.log(import.meta.env.VITE_USER_PASSWORD);
 export default user;
